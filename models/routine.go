4c22bce3d018bc2bfe4455f0822eb7a4a05cd376
GiwrgosGln <gergegl1999@gmail.com>
2024-01-25 19:06:56 +0200

    Changed Routines

--- models/routine.go
+++ models/routine.go
@@ -1,32 +1,13 @@
+// routine.go
 package models
-
-// SetType represents the type of a set.
-type SetType string
-
-const (
-	WarmUp   SetType = "warm-up"
-	Working  SetType = "working"
-	Failure  SetType = "failure"
-	Drop     SetType = "drop"
-)
-
-// Set represents a set in a routine exercise.
-type Set struct {
-	SetType SetType `json:"setType"`
-	Sets    int     `json:"sets"`
-}
-
-// RoutineExercise represents an exercise in a routine.
-type RoutineExercise struct {
-	ExerciseName string `json:"exerciseName"`
-	Sets         []Set  `json:"sets"`
-}
 
 // Routine represents a routine document.
 type Routine struct {
-	ID           string            `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserID       string            `json:"userID"`
-	Name         string            `json:"name"`
-	Description  string            `json:"description"`
-	Exercises    []RoutineExercise `json:"exercises"`
+	ID        string           `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID    string           `json:"userID"`
+	Title     string           `json:"title"`
+	Exercises map[string][]SetType `json:"exercises"`
 }
+
+// SetType represents the type of a set.
+type SetType string
