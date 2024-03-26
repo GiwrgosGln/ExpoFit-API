110e6ebaee6ff0b6c8bd0d6a10896030917e8b64
Giwrgos.Gln <gergegl1999@gmail.com>
2024-03-26 21:52:11 +0200

    change routine model

--- models/routine.go
+++ models/routine.go
@@ -3,11 +3,12 @@ package models
 
 // Routine represents a routine document.
 type Routine struct {
-	ID        string           `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserID    string           `json:"userID"`
-	Title     string           `json:"title"`
-	Exercises map[string][]SetType `json:"exercises"`
+	ID        string     `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID    string     `json:"userID"`
+	Title     string     `json:"title"`
+	Exercises []Exercise `json:"exercises"`
 }
+
 
 // SetType represents the type of a set.
 type SetType string
