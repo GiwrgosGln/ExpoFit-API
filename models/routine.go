971617a9ae7c16b4878d5d0731fe78cc0c1f4821
Giwrgos.Gln <gergegl1999@gmail.com>
2024-03-26 20:47:02 +0200

    update routine model

--- models/routine.go
+++ models/routine.go
@@ -3,11 +3,20 @@ package models
 
 // Routine represents a routine document.
 type Routine struct {
-	ID        string           `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserID    string           `json:"userID"`
-	Title     string           `json:"title"`
-	Exercises map[string][]SetType `json:"exercises"`
+	ID        string              `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID    string              `json:"userID"`
+	Title     string              `json:"title"`
+	Exercises []Exercise          `json:"exercises"`
 }
 
-// SetType represents the type of a set.
-type SetType string
+// Exercise represents an exercise.
+type Exercise struct {
+	Name             string             `json:"name"`
+	BodyPart         string             `json:"bodyPart"`
+	Equipment        string             `json:"equipment"`
+	GifURL           string             `json:"gifURL"`
+	ID               string             `json:"id"`
+	Instructions     []string           `json:"instructions"`
+	SecondaryMuscles []string           `json:"secondaryMuscles"`
+	Target           string             `json:"target"`
+}
