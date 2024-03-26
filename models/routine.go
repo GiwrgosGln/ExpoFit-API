bae055d280b072908fdc5b1197b24ed1ee33d3ce
Giwrgos.Gln <gergegl1999@gmail.com>
2024-03-26 21:07:11 +0200

    bug fix

--- models/routine.go
+++ models/routine.go
@@ -3,20 +3,11 @@ package models
 
 // Routine represents a routine document.
 type Routine struct {
-	ID        string              `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserID    string              `json:"userID"`
-	Title     string              `json:"title"`
-	Exercises []Exercise          `json:"exercises"`
+	ID        string           `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID    string           `json:"userID"`
+	Title     string           `json:"title"`
+	Exercises map[string][]SetType `json:"exercises"`
 }
 
-// Exercise represents an exercise.
-type Exercise struct {
-	Name             string             `json:"name"`
-	BodyPart         string             `json:"bodyPart"`
-	Equipment        string             `json:"equipment"`
-	GifURL           string             `json:"gifURL"`
-	ID               string             `json:"id"`
-	Instructions     []string           `json:"instructions"`
-	SecondaryMuscles []string           `json:"secondaryMuscles"`
-	Target           string             `json:"target"`
-}
+// SetType represents the type of a set.
+type SetType string
