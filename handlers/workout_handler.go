f0118d34d00a182252155e37f700b85878bb334b
Giwrgos.Gln <gergegl1999@gmail.com>
2024-04-06 11:12:31 +0300

    add workout model and handler

--- handlers/workout_handler.go
+++ handlers/workout_handler.go
@@ -9,47 +9,8 @@ import (
 	"FitnessAPI/models"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
-
-// GetWorkoutsByUserIDHandler handles the GET request to fetch workouts by user ID.
-func GetWorkoutsByUserIDHandler(c *gin.Context, collection *mongo.Collection) {
-	userID := c.Param("userID") // Extract the user ID from the request path
-
-	// Define a filter to query workouts by user ID
-	filter := bson.M{"user_id": userID}
-
-	// Execute the find operation to get workouts matching the filter
-	cursor, err := collection.Find(context.Background(), filter)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch workouts"})
-		return
-	}
-	defer cursor.Close(context.Background())
-
-	// Initialize a slice to store fetched workouts
-	var workouts []models.Workout
-
-	// Iterate through the cursor and decode each document into a Workout struct
-	for cursor.Next(context.Background()) {
-		var workout models.Workout
-		if err := cursor.Decode(&workout); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode workout"})
-			return
-		}
-		workouts = append(workouts, workout)
-	}
-
-	// Check if any error occurred during cursor iteration
-	if err := cursor.Err(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cursor iteration error"})
-		return
-	}
-
-	// Return the fetched workouts as a response
-	c.JSON(http.StatusOK, workouts)
-}
 
 // SaveWorkoutHandler handles the POST request to save a workout.
 func SaveWorkoutHandler(c *gin.Context, collection *mongo.Collection) {
--- models/workout.go
+++ models/workout.go
@@ -15,7 +15,6 @@ type Workout struct {
 	ID        string               `json:"id" bson:"_id,omitempty"`
 	UserID    string               `json:"user_id" bson:"user_id"`
 	Date      time.Time            `json:"date"`
-	RoutineName string               `json:"routine_name" bson:"routine_name"`
 	Exercises []ExerciseInWorkout `json:"exercises"`
 }
 
