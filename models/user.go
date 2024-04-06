97ef399510d31e03cd57812864cde1a47d3a5941
Giwrgos.Gln <gergegl1999@gmail.com>
2024-04-06 09:59:59 +0300

    update user model

--- models/user.go
+++ models/user.go
@@ -1,8 +1,9 @@
 package models
 
 type User struct {
-	ID       interface{} `json:"id" bson:"_id,omitempty"`
-	Username string      `json:"username"`
-	Sex      string      `json:"sex"`
-	Weight   int         `json:"weight"`
+	ID          interface{} `json:"id" bson:"_id,omitempty"`
+	Username    string      `json:"username"`
+	Email       string      `json:"email"`
+	Gender      string      `json:"gender,omitempty"`
+	DateOfBirth string      `json:"date_of_birth,omitempty"`
 }
