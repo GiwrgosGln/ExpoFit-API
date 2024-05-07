6c970e1e764e2ebf3ba18f19beed9dd75e3a2a5a
Giwrgos.Gln <gergegl1999@gmail.com>
2024-05-07 10:31:36 +0300

    fix for deploying in production

--- database/mongo.go
+++ database/mongo.go
@@ -3,21 +3,19 @@ package database
 
 import (
 	"context"
-	"log"
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // ConnectMongoDB connects to MongoDB and initializes the specified collection
 func ConnectMongoDB(collectionName string) (*mongo.Collection, error) {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	// Load MongoDB URI and Database Name from the environment variables
 	mongoURI := os.Getenv("MONGODB_URI")
