b737471a720de5db54fb1105f0ae40aa96bc5bef
GiwrgosGln <gergegl1999@gmail.com>
2024-05-09 14:00:35 +0300

    change measurements model

--- models/measurements.go
+++ models/measurements.go
@@ -6,8 +6,6 @@ import "time"
 type Measurement struct {
 	ID         string    `json:"_id,omitempty" bson:"_id,omitempty"`
 	UserID     string    `json:"user_id,omitempty" bson:"user_id,omitempty"`
-	Height     float64   `json:"height,omitempty" bson:"height,omitempty"`
 	BodyWeight float64   `json:"body_weight,omitempty" bson:"body_weight,omitempty"`
-	BodyFat    float64   `json:"body_fat,omitempty" bson:"body_fat,omitempty"`
 	Date       time.Time `json:"date,omitempty" bson:"date,omitempty"`
 }
