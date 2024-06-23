cad96915c9ce585473a08647b48bf9462366ad1d
GiwrgosGln <gergegl1999@gmail.com>
2024-06-23 21:46:33 +0300

    remove comments

--- database/mongo.go
+++ database/mongo.go
@@ -1,4 +1,3 @@
-// database/mongogo.go
 package database
 
 import (
--- models/workout.go
+++ models/workout.go
@@ -2,7 +2,6 @@ package models
 
 import "time"
 
-// ExerciseInWorkout represents an exercise within a workout, excluding unnecessary fields.
 type ExerciseInWorkout struct {
 	ID     string `json:"_id" bson:"_id,omitempty"`
 	ExerciseID string `json:"exercise_id" bson:"exercise_id"`
@@ -10,7 +9,6 @@ type ExerciseInWorkout struct {
 	Sets   []Set  `json:"sets" bson:"sets"`
 }
 
-// Workout represents a workout.
 type Workout struct {
 	ID        string               `json:"id" bson:"_id,omitempty"`
 	UserID    string               `json:"user_id" bson:"user_id"`
@@ -19,7 +17,6 @@ type Workout struct {
 	Exercises []ExerciseInWorkout `json:"exercises"`
 }
 
-// Set represents a set within an exercise.
 type Set struct {
 	Type   string `json:"type"`
 	Reps   *int    `json:"reps"`
